f10ad2917ae73b2fda38c54737906c0f7286646e
unsoluble <unsoluble@me.com>
2021-04-28 07:26:21 +0000

    Pin & jiggle

--- scripts/smalltime.js
+++ scripts/smalltime.js
@@ -6,15 +6,51 @@ Hooks.on('init', () => {
     scope: 'world',
     config: false,
     type: Number,
-    default: 0,
- });
- game.settings.register('smallTime', 'position', {
+    default: 0
+  });
+  
+  game.settings.register('smallTime', 'position', {
     name: 'Position',
     scope: 'world',
     config: false,
     type: Object,
-    default: { top: 446, left: 20 },
- });
+    default: { top: 446, left: 20 }
+  });
+  
+  game.settings.register('smallTime', 'pinned', {
+    name: 'Pinned',
+    scope: 'world',
+    config: false,
+    type: Boolean,
+    default: true
+  });
+  
+  game.settings.register('smallTime', 'visible', {
+    name: 'Visible',
+    scope: 'world',
+    config: false,
+    type: Boolean,
+    default: true
+  });
+});
+
+Hooks.on("getSceneControlButtons", (buttons) => {
+  if (!canvas)
+    return
+  let group = buttons.find(b => b.name == "notes");
+  group.tools.push({
+    button: true,
+    icon: "fas fa-adjust",
+    name: "smalltime",
+    title: "SmallTime",
+    onClick: () => {
+      if (false) {
+        new SmallTimeApp().render(true);
+      } else {
+        SmallTimeApp().close();
+      }
+    },
+  });
 });
 
 Hooks.on('renderPlayerList', () => {
@@ -30,20 +66,12 @@ Hooks.on('renderPlayerList', () => {
 });
 
 Hooks.on('ready', () => {
-  new SmallTimeApp().render(true);
-  
-  const element = document.getElementById('players');
-  const playerAppPos = element.getBoundingClientRect();
-  const myOffset = playerAppPos.height + 88;
-  
-  $('body').append(`
-    <style id="pin-lock">
-      #smalltime-app {
-        top: calc(100vh - ${myOffset}px) !important;
-        left: 15px !important;
-      }
-    </style>
-  `);
+  if (game.settings.get('smallTime', 'visible')) {
+    new SmallTimeApp().render(true);
+  }
+  if (game.settings.get('smallTime', 'pinned')) {
+    pinApp();
+  }
 });
 
 class SmallTimeApp extends FormApplication {
@@ -53,10 +81,18 @@ class SmallTimeApp extends FormApplication {
   }
 
   static get defaultOptions() {
+    
+    const pinned = game.settings.get('smallTime', 'pinned');
+    
+    const playerApp = document.getElementById('players');
+    const playerAppPos = playerApp.getBoundingClientRect();
+    
     this.initialPosition = game.settings.get('smallTime', 'position');
     
-    const element = document.getElementById('players');
-    const playerAppPos = element.getBoundingClientRect();
+    if (pinned) {
+      this.initialPosition.top = playerAppPos.top - 70;
+      this.initialPosition.left = playerAppPos.left;
+    }
     
     return mergeObject(super.defaultOptions, {
       classes: ['form'],
@@ -66,8 +102,8 @@ class SmallTimeApp extends FormApplication {
       template: 'modules/smalltime/templates/floater.html',
       id: 'smalltime-app',
       title: 'SmallTime',
-      top: playerAppPos.top - 70, // this.initialPosition.top,
-      left: playerAppPos.left, // this.initialPosition.left,
+      top: this.initialPosition.top,
+      left: this.initialPosition.left
     });
   }
   
@@ -84,54 +120,103 @@ class SmallTimeApp extends FormApplication {
     const dragHandle = html.find('#dragHandle')[0];
     const drag = new Draggable(this, html, dragHandle, false);
     
-    let pinLock = true;
+    let pressTimer = null;
+    let pinLock = game.settings.get('smallTime', 'pinned');
+    let pinZone = false;
+    
+    drag._onDragMouseDown = function _newOnDragMouseDown(event) {
+      event.preventDefault();
+      
+      // Record initial position
+      this.position = duplicate(this.app.position);
+      this._initial = {x: event.clientX, y: event.clientY};
+      
+      pinLock = game.settings.get('smallTime', 'pinned');
+      
+      if (pinLock) {
+        pressTimer = setTimeout(function() {
+          $('#smalltime-app').css("animation", "jiggle 0.2s infinite");
+          unPinApp();
+          pinLock = false;
+        },500);
+      }
+      window.addEventListener(...this.handlers.dragMove);
+      window.addEventListener(...this.handlers.dragUp);
+    }
     
     drag._onDragMouseMove = function _newOnDragMouseMove(event) {
       event.preventDefault();
       
-      if (pinLock == true) {
-        $('#smalltime-app').css("animation", "jiggle 0.2s infinite");
-        if ( ( (event.clientX - this._initial.x) > 100 ) || ( (event.clientY - this._initial.y) > 100 ) ) {
-          $('#pin-lock').remove();
-          pinLock = false;
-          $('#smalltime-app').css("animation", "none");
-        }
-      }
+      const playerApp = document.getElementById('players');
+      const playerAppPos = playerApp.getBoundingClientRect();
+      
+      clearTimeout(pressTimer);
       
       // Limit dragging to 60 updates per second
       const now = Date.now();
       if ( (now - this._moveTime) < (1000/60) ) return;
       this._moveTime = now;
-  
+      
       // Update application position, but only if the app is unpinned
       if (pinLock == false) {
         this.app.setPosition({
           left: this.position.left + (event.clientX - this._initial.x),
           top: this.position.top + (event.clientY - this._initial.y)
         });
+        
+        let playerAppUpperBound = playerAppPos.top - 50;
+        let playerAppLowerBound = playerAppPos.top + 50;
+        
+        if ( ( event.clientX < 215 ) && ( ( event.clientY > playerAppUpperBound ) && ( event.clientY < playerAppLowerBound ) ) ) {
+          $('#smalltime-app').css("animation", "jiggle 0.2s infinite");
+          pinZone = true;
+        } else {
+          $('#smalltime-app').css("animation", "");
+          pinZone = false;
+        }
       }
     }
     
     drag._onDragMouseUp = function _newOnDragMouseUp(event) {
       event.preventDefault();
+      
       window.removeEventListener(...this.handlers.dragMove);
       window.removeEventListener(...this.handlers.dragUp);
-      let windowPos = $('#smalltime-app').position();
-      let newPos = { top: windowPos.top, left: windowPos.left };
-      game.settings.set('smallTime', 'position', newPos);
-      $('#smalltime-app').css("animation", "none");
+      
+      clearTimeout(pressTimer);
+      
+      const playerApp = document.getElementById('players');
+      const playerAppPos = playerApp.getBoundingClientRect();
+      const myOffset = playerAppPos.height + 88;
+      
+      if (pinZone) {
+        pinApp();
+        game.settings.set('smallTime', 'pinned', true);
+        this.app.setPosition({
+          left: 15,
+          top: window.innerHeight - myOffset
+        });
+      } else {
+        let windowPos = $('#smalltime-app').position();
+        let newPos = { top: windowPos.top, left: windowPos.left };
+        game.settings.set('smallTime', 'position', newPos);
+        game.settings.set('smallTime', 'pinned', false);
+      }
+      
+      $('#smalltime-app').css("animation", "");
     }
     
     timeTransition(this.currentTime);
-	
+  
     $(document).on('input', '#timeSlider', function() {
       $('#timeDisplay').html( convertTime( $(this).val() ) );
       
       timeTransition( $(this).val() );
     });
-
+    
+    
   }
-  
+    
   async _updateObject(event, formData) {
     // Get the slider value
     const newTime = formData.timeSlider;
@@ -141,6 +226,29 @@ class SmallTimeApp extends FormApplication {
     // Save the new time
     await game.settings.set('smallTime', 'currentTime', newTime);
   }
+}
+
+function pinApp() {
+  if ( !($("#pin-lock").length) ) {
+    const playerApp = document.getElementById('players');
+    const playerAppPos = playerApp.getBoundingClientRect();
+    const myOffset = playerAppPos.height + 88;
+  
+    $('body').append(`
+      <style id="pin-lock">
+        #smalltime-app {
+          top: calc(100vh - ${myOffset}px) !important;
+          left: 15px !important;
+        }
+      </style>
+    `);
+    
+    game.settings.set('smallTime', 'pinned', true);
+  }
+}
+
+function unPinApp() {
+  $('#pin-lock').remove();
 }
 
 function timeTransition(timeNow) {
