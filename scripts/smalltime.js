288f8ec4c1c1ee3933cd719176b95cd65f01f48d
unsoluble <unsoluble@me.com>
2021-06-13 06:02:12 +0000

    SC index fix again

--- scripts/smalltime.js
+++ scripts/smalltime.js
@@ -1592,7 +1592,7 @@ class SmallTimeApp extends FormApplication {
       let SCobject = SimpleCalendar.api.timestampToDate(game.time.worldTime);
       newDay = SCobject.weekdays[SCobject.dayOfTheWeek];
       newMonth = SCobject.monthName;
-      newDate = SCobject.day;
+      newDate = SCobject.day + 1;
       newYear = SCobject.year;
       displayDate = newDay + ', ' + newMonth + ' ' + newDate + ', ' + newYear;
     }
